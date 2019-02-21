45666f4f7802d8714aa48dd245839c08446bd1bc
huangkai <18778335525@163.com>
2019-02-21 23:20:27 +0800

    初步界面框架

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -10,6 +10,7 @@ import {ArticleService} from '../article.service';
 export class ArticleComponent implements OnInit {
   articles: Article[];
   selectArticle: Article;
+  data: any[] = [];
 
   constructor(
     private articleService: ArticleService
@@ -36,6 +37,20 @@ export class ArticleComponent implements OnInit {
 
   ngOnInit() {
     this.getArticles();
+    this.loadData(1);
   }
 
+  loadData(pi: number): void {
+    this.data = new Array(5).fill({}).map((i, index) => {
+      return {
+        href: 'http://ant.design',
+        title: `ant design part ${index} (page: ${pi})`,
+        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+        description: 'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), ' +
+          'to help people create their product prototypes beautifully and efficiently.'
+      };
+    });
+
+  }
 }
